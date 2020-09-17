d725c4d197fe1d95bad5b2bd2339d8732da46057
Ivan Glebov <glebov.vanya8@gmail.com>
2020-09-17 20:20:25 +0300

    used examples code to find out wtf is with data transfer - data transfer is still not working(

--- I2C_Mater_ESP8266/src/main.cpp
+++ I2C_Mater_ESP8266/src/main.cpp
@@ -1,5 +1,5 @@
 // MASTER on I2C bus code
-
+/*
 #include <Arduino.h>
 #include <Wire.h>
 
@@ -20,14 +20,40 @@ void setup() {
 }
 
 void loop() {
-  
+
   counter++;
   // First arg is slave ID, second arg - number of bytes we are going te read
   Wire.requestFrom(0x01, 1); // Request data from board with addred 0x01
   if (Wire.available()){ // If data arrived
     i2c_rcv = Wire.read();
     // We revieved one byte of data 
+    Serial.println("Recieved byte : " + String(i2c_rcv));
   }
-  Serial.println("Recieved byte : " + String(i2c_rcv));
-
+  
+  delay(1000);
 }
+
+*/
+
+
+// MASTER
+#include <Arduino.h>
+#include <Wire.h>
+
+void setup() {
+  Wire.begin();        // join i2c bus (address optional for master)
+  Serial.begin(115200);  // start serial for output
+}
+
+void loop() {
+  Wire.requestFrom(8, 6);    // request 6 bytes from slave device #8
+
+  while (Wire.available()) { // slave may send less than requested
+    char c = Wire.read(); // receive a byte as character
+    Serial.print(c);         // print the character
+  }
+
+  delay(500);
+}
+
+
--- I2C_Slave_ESP8266/src/main.cpp
+++ I2C_Slave_ESP8266/src/main.cpp
@@ -1,4 +1,5 @@
 // SLAVE I2C bus device code
+/*
 #include <Arduino.h>
 #include <Wire.h>
 
@@ -7,12 +8,14 @@ unsigned long time_start;
 int statusLED;
 byte counter;
 
+void dataRcv(int);
 void dataRqst();
 
 void setup() {
-  Wire.begin(0x01); // Join I2C bus with 0x01 address
+  Wire.begin(40); // Join I2C bus with 0x01 address
+  Wire.onReceive(dataRcv);
   Wire.onRequest(dataRqst);
-  
+
 }
 
 void loop() {
@@ -21,14 +24,40 @@ void loop() {
   if(millis() - t >= 1000) {
     counter++;
     t = millis();
+    if (counter > 255)
+      counter = 0;
   }
   
 }
 
-
+void dataRcv(int numBytes){
+  while(Wire.available()){
+    i2c_rcv = Wire.read();
+  }
+}
 
 void dataRqst(){
   Wire.write(counter);
 }
+*/
 
+// SLAVE
+#include <Arduino.h>
+#include <Wire.h>
+void requestEvent();
+void setup() {
+  Wire.begin(8);                // join i2c bus with address #8
+  Wire.onRequest(requestEvent); // register event
+}
+
+void loop() {
+  delay(100);
+}
+
+// function that executes whenever data is requested by master
+// this function is registered as an event, see setup()
+void requestEvent() {
+  Wire.write("hello "); // respond with message of 6 bytes
+  // as expected by master
+}
 

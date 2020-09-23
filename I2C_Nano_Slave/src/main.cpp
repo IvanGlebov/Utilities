d0bbee786f5797cb1b6bfbe4b1b09217f553a96d
Ivan Glebov <glebov.vanya8@gmail.com>
2020-09-23 17:44:38 +0300

    Added structure for packet variables saving and added function to convert structure to string for sending

--- I2C_Nano_Slave/src/main.cpp
+++ I2C_Nano_Slave/src/main.cpp
@@ -2,9 +2,18 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <math.h>
+// Structure for packet variables saving
+struct packetData{
+  int id;
+  float airTemp;
+  float airHum;
+  float groundTemp;
+  float groundHum;
+  float lightLevel;
+};
 
 void requestEvent();
-
+String structFormPacket(packetData d1);
 String formPacket(int id, float tempAir, float humAir, float tempGround, float humGround, float lightLevel);
 
 void setup() {
@@ -14,15 +23,85 @@ void setup() {
 }
 
 void loop() {
-  // delay(100);
+  // Do nothing while no signals from master
 }
 
-
-String formPacket(int id, float tempAir, float humAir, float tempGround, float humGround, float lightLevel){
+String structFormPacket(packetData d1){
   String temp = "";
   double l, r;
   // int lint, rint;
-  char temp1[5] = "";
+  // char temp1[5] = "";
+
+  // ID adding to string
+  if(d1.id < 10) temp += "0" + String(d1.id);
+  else temp += String(d1.id);
+
+  // Air temperature adding to string
+  if(d1.airTemp >= 0) temp += "+";
+  else temp += "-";
+  //  abs(x) ((x)>0?(x):-(x))
+  float airTemp = abs(d1.airTemp);
+  l = int(airTemp); // Left part of the number
+  r = airTemp - l; // right part of the nember
+  r *= 100; // Convert r value to full integer
+  r = int(r);
+  if(l < 10) temp += "0" + String(static_cast<int>(l)) /* + "/" */;
+  else if (l < 100) temp += String(static_cast<int>(l))/*  + "/" */;
+  if(r < 10) temp += "0" + String(static_cast<int>(r))/*  + ";" */;
+  else if (r < 100)  temp += String(static_cast<int>(r))/*  + ";" */;
+
+  // Air humidity adding to string
+  l = int(d1.airHum);
+  r = int((d1.airHum-l)*100);
+  if(l < 10) temp += "00" + String(static_cast<int>(l))/*  + "/" */;
+  else if (l < 100) temp += "0" + String(static_cast<int>(l))/*  + "/" */;
+  else if (l == 100) temp += "100/";
+  if(r < 10) temp += "0" + String(static_cast<int>(r))/*  + ";" */;
+  else if (r < 100)  temp += String(static_cast<int>(r))/*  + ";" */;
+
+  //tempGround
+  if(d1.groundTemp >= 0) temp += "+";
+  else temp += "-";
+  //  abs(x) ((x)>0?(x):-(x))
+  float groundTemp = abs(d1.groundTemp);
+  l = int(groundTemp); // Left part of the number
+  r = groundTemp - l; // right part of the nember
+  r *= 100; // Convert r value to full integer
+  r = int(r);
+  // temp += "T";
+  if(l < 10) temp += "0" + String(static_cast<int>(l))/*  + "/" */;
+  else if (l < 100) temp += String(static_cast<int>(l))/*  + "/" */;
+  if(r < 10) temp += "0" + String(static_cast<int>(r))/*  + ";" */;
+  else if (r < 100)  temp += String(static_cast<int>(r))/*  + ";" */;
+
+  // Ground humidity adding to string
+  l = int(d1.groundHum);
+  r = int((d1.groundHum-l)*100);
+  if(l < 10) temp += "00" + String(static_cast<int>(l))/*  + "/" */;
+  else if (l < 100) temp += "0" + String(static_cast<int>(l))/*  + "/" */;
+  else if (l == 100) temp += "100" /* / */;
+  if(r < 10) temp += "0" + String(static_cast<int>(r))/*  + ";" */;
+  else if (r < 100)  temp += String(static_cast<int>(r))/*  + ";" */;
+
+  // Light level adding to string
+  l = int(d1.lightLevel);
+  r = int((d1.lightLevel-l)*100);
+  if(l < 10) temp += "000" + String(static_cast<int>(l))/* + "/"*/;
+  else if (l < 100) temp += "00" + String(static_cast<int>(l))/* + "/"*/;
+  else if (l < 1000) temp += "0" + String(static_cast<int>(l))/* + "/"*/;
+  else if (l < 10000) temp += String(static_cast<int>(l))/* + "/"*/;
+  else temp += "9999/";
+  if(r < 10) temp += "0" + String(static_cast<int>(r))/* + ";"*/;
+  else if (r < 100)  temp += String(static_cast<int>(r))/* + ";"*/;
+
+  return temp;
+}
+String formPacket(int id, float tempAir, float humAir, float tempGround, float humGround, float lightLevel){
+
+  String temp = "";
+  double l, r;
+  // int lint, rint;
+  // char temp1[5] = "";
 
   // ID adding to string
   if(id < 10) temp += "0" + String(id);
@@ -37,9 +116,6 @@ String formPacket(int id, float tempAir, float humAir, float tempGround, float h
   r = airTemp - l; // right part of the nember
   r *= 100; // Convert r value to full integer
   r = int(r);
-  // temp += "t";
-  // lint = l;
-  // rint = r;
   if(l < 10) temp += "0" + String(static_cast<int>(l)) /* + "/" */;
   else if (l < 100) temp += String(static_cast<int>(l))/*  + "/" */;
   if(r < 10) temp += "0" + String(static_cast<int>(r))/*  + ";" */;
@@ -48,9 +124,6 @@ String formPacket(int id, float tempAir, float humAir, float tempGround, float h
   // Air humidity adding to string
   l = int(humAir);
   r = int((humAir-l)*100);
-  // temp += "h";
-  // lint = l;
-  // rint = r;
   if(l < 10) temp += "00" + String(static_cast<int>(l))/*  + "/" */;
   else if (l < 100) temp += "0" + String(static_cast<int>(l))/*  + "/" */;
   else if (l == 100) temp += "100/";
@@ -75,7 +148,6 @@ String formPacket(int id, float tempAir, float humAir, float tempGround, float h
   // Ground humidity adding to string
   l = int(humGround);
   r = int((humGround-l)*100);
-  // temp += "H";
   if(l < 10) temp += "00" + String(static_cast<int>(l))/*  + "/" */;
   else if (l < 100) temp += "0" + String(static_cast<int>(l))/*  + "/" */;
   else if (l == 100) temp += "100" /* / */;
@@ -85,7 +157,6 @@ String formPacket(int id, float tempAir, float humAir, float tempGround, float h
   // Light level adding to string
   l = int(humGround);
   r = int((humGround-l)*100);
-  // temp += "l";
   if(l < 10) temp += "000" + String(static_cast<int>(l))/* + "/"*/;
   else if (l < 100) temp += "00" + String(static_cast<int>(l))/* + "/"*/;
   else if (l < 1000) temp += "0" + String(static_cast<int>(l))/* + "/"*/;
@@ -94,45 +165,25 @@ String formPacket(int id, float tempAir, float humAir, float tempGround, float h
   if(r < 10) temp += "0" + String(static_cast<int>(r))/* + ";"*/;
   else if (r < 100)  temp += String(static_cast<int>(r))/* + ";"*/;
 
-/*
-  // Device id
-  sprintf(temp1, "%02d", id);
-  temp += String(temp1[0]) + String(temp1[1]);
-  
-  // Air temperature parametrs
-  if (tempAir >= 0.0) temp += "+";
-  else temp += "-";
-  r = modf(tempAir, &l);
-  sprintf(temp1, "%02d", l);
-  temp += String(temp[0]) + String(temp[1]);
-  temp += "/";
-  
-  sprintf(temp1, "%02d", r);
-  temp += String(temp1[0]) + String(temp1[1]);
-  temp += ";";
-  // Air humidity parametrs
-  temp += "h";
-  r = modf(humAir, &l);
-  sprintf(temp1, "%03d", l);
-  temp += String(temp1[0]) + String(temp1[1]) + String(temp1[2]);
-  temp += "/";
-  sprintf(temp1, "%02d", r);
-  temp += String(temp1[0]) + String(temp1[1]);
-  temp += ";";
-  */
-
-  // temp += "\r";
   return temp;
 }
 
 // function that executes whenever data is requested by master
 // this function is registered as an event, see setup()
 void requestEvent() {
-  String packet = formPacket(1, 1.1, 1.1, 1.1, 1.1, 1.1);
+  packetData data1;
+  data1.airHum = 11.2;
+  data1.airTemp = 24.3;
+  data1.groundHum = 45.2;
+  data1.groundTemp = 23.8;
+  data1.id = 1.0;
+  data1.lightLevel = 1297.45;
+  String packet1 = structFormPacket(data1);
+  // String packet = formPacket(1, 1.1, 1.1, 1.1, 1.1, 1.1);
   int len = 28;
   for(int i=0; i < len; i++){
-    Wire.write(packet[i]);
-    Serial.print(packet[i]);
+    Wire.write(packet1[i]);
+    Serial.print(packet1[i]);
   }
   Serial.println();
   //Wire.write("hello"); // respond with message of 6 bytes
